fe05b25df1e80b91332d4285936246af7013677c
Arrietty1219 <158137190+Arrietty1219@users.noreply.github.com>
2024-04-17 14:32:03 +0900

    kadai_005をプッシュ

---
+++ kadai_005/score.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>kadai_005</title>
+</head>
+
+<body>
+    <p>
+        <?php
+        // 変数作成と値代入
+        $score1 = 80;
+        $score2 = 60;
+        $score3 = 55;
+        $score4 = 40;
+        $score5 = 100;
+        $score6 = 25;
+        $score7 = 80;
+        $score8 = 95;
+        $score9 = 30;
+        $score10 = 60;
+
+        //合計点の算出
+        $sum = $score1 + $score2 + $score3 + $score4 + $score5 + $score6 + $score7 + $score8 + $score9 + $score10;
+        
+        //平均点の算出
+        echo $sum / 10;
+        ?>
+    </p>
+</body>
+
+</html>
