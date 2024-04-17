185d18ca5d066990f011aa5f957b7db741adbad8
Arrietty1219 <158137190+Arrietty1219@users.noreply.github.com>
2024-04-17 15:17:07 +0900

    kadai_007をプッシュ

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>kadai_007</title>
+</head>
+
+<body>
+    <p>
+        <?php
+        // 連想配列作成と変数への値代入
+        $product_detail = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+
+        //出力
+        print_r($product_detail);
+        ?>
+    </p>
+</body>
+
+</html>
